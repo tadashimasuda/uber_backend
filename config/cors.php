f4279edda273dfd6a65f6b7f3d886e1c36751f21
tadashimasuda <tmasu4260@gmail.com>
2020-11-21 00:23:04 +0900

    edit origin

--- config/cors.php
+++ config/cors.php
@@ -31,7 +31,7 @@ return [
     /*
      * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    'allowed_origins' => ['https://uber-post.netlify.app'],
+    'allowed_origins' => ['https://uber-post.netlify.app','http://localhost:3000'],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
