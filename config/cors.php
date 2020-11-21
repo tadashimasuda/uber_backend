1b26309922a7d1abe578214c35d9b2bb4285ada3
tadashimasuda <tmasu4260@gmail.com>
2020-11-21 09:20:42 +0900

    add headers

--- config/cors.php
+++ config/cors.php
@@ -41,7 +41,7 @@ return [
     /*
      * Sets the Access-Control-Allow-Headers response header. `['*']` allows all headers.
      */
-    'allowed_headers' => ['Content-Type'],
+    'allowed_headers' => ['Content-Type, Authorization, Accept'],
 
     /*
      * Sets the Access-Control-Expose-Headers response header with these headers.
